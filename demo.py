a290f37b38c7da09340e81524038bce0af18c17c
Mary Huang <rhymeandreason1.0@gmail.com>
2014-07-29 14:38:45 -0400

    Main portions of page 3

---
+++ demo.py
@@ -0,0 +1,29 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+# We set a parent key on the 'Greetings' to ensure that they are all in the same
+# entity group. Queries across the single entity group will be consistent.
+# However, the write rate should be limited to ~1/second.
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render())
+
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
