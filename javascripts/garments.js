6e3e001824ff82ffc6c3713706f09c511da343bc
Mary Huang <rhymeandreason1.0@gmail.com>
2014-07-29 21:22:32 -0400

    added javascripts folder and Three files

---
+++ javascripts/garments.js
@@ -0,0 +1,186 @@
+var container, stats;
+
+var camera, scene, renderer;
+
+var geometry, objects;
+var material, meshOutline, shader1, shader2, light;
+
+
+
+var geometry, mesh, body;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var mousePressed = false;
+var mouseX = 0, mouseY = 0;
+var mousex = 0, mousey= 0, pmousex= 0, pmousey= 0, dx= 0, dy= 0;
+	dx = getPositionLeft(document.getElementById("container")) - 5;
+	dy = getPositionTop(document.getElementById("container")) - 5;
+
+	var yamt = 0, xamt = 0;
+
+
+init();
+animate();
+
+document.onmousedown = function() {
+mousePressed = true;
+}
+document.onmouseup = function() {
+mousePressed = false;
+}
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 15000 );
+	camera.position.z = 200;
+	scene.add( camera );
+	
+
+	var ambient = new THREE.AmbientLight(0x0C0C0C);
+	scene.add(ambient);
+
+
+	var directionalLight = new THREE.DirectionalLight(0xD6D6D6);
+	directionalLight.position.set(70, 50, 100);
+	scene.add(directionalLight);
+
+	var directionalLight2 = new THREE.DirectionalLight(0xD6D6D6);
+	directionalLight2.position.set(-70, 50, 100);
+	scene.add(directionalLight2);
+	
+	var material1 = new THREE.MeshLambertMaterial( { color: 0x202020, morphTargets: true, side: THREE.DoubleSide} );
+	
+	var material3 = new THREE.MeshLambertMaterial( { color: 0xFFFFFF, morphTargets: false, side: THREE.DoubleSide} );
+	
+	var material2 =  new THREE.MeshBasicMaterial({ color: 0xFFFFFF, morphTargets: true, shading: THREE.FlatShading, wireframe: true, wireframeLinewidth:1.5, opacity:0.3, transparent: true });
+
+
+	//var geometry = new THREE.CubeGeometry( 100, 100, 100 );
+	var loader = new THREE.JSONLoader( true );
+    loader.load( "mens_coat.js", function( geometry ) {
+
+			mesh = new THREE.Mesh( geometry, material1  );
+			// mesh = THREE.SceneUtils.createMultiMaterialObject( geometry, [
+      //    material1,
+      //    material2
+      //]);
+			
+			mesh.scale.set( 9, 9, 9 );
+			scene.add( mesh );
+			//mesh.add( body );
+    
+	} );
+	
+	var loader2 = new THREE.JSONLoader( true );
+    loader.load( "male_model.js", function( geometry ) {
+
+			body = new THREE.Mesh( geometry, material3  );
+			
+			body.scale.set( 9, 9, 9 );
+			scene.add(body);
+    
+	} );
+	
+  renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( 0xf0f0f0 );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	renderer.sortObjects = false;
+	container.appendChild( renderer.domElement );
+	
+	container.onmousemove = function(event) {
+		if (mousePressed){
+			pmousex = mousex;
+			pmousey = mousey;
+
+			mousex = event.pageX - dx;
+			mousey = event.pageY - dy;
+			ease = 0.9;
+		if (mousex-pmousex>0){
+			yamt = .1;
+		} else {
+			yamt = -.1;
+		}
+
+		if (mousey-pmousey>0){
+			xamt = .02;
+		} else {
+			xamt = -.02;
+		}
+
+		if (Math.abs(mousey-pmousey) > Math.abs(mousex-pmousex)) {
+			mesh.rotation.x += xamt;
+			body.rotation.x += xamt;
+			//back.rotation.x += xamt;
+		} else {
+			mesh.rotation.y += yamt;
+			body.rotation.y += yamt;
+			//back.rotation.y += yamt;
+		}
+
+		}
+
+	}
+
+	container.onmouseup = function() {
+		//var rot = ((garment.rotation.y+Math.PI)/(Math.PI*2)) *360;
+		//$("#rotate-slider").slider('value', rot);
+	}
+
+	container.onmouseover = function(event) {
+		mouseIsOver = true;
+	}
+
+	container.onmouseout = function(event) {
+		mouseIsOver = false;
+	}
+
+}
+
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+
+	//mesh.rotation.y += 0.01;
+
+	//mesh.morphTargetInfluences[ 0 ] = Math.sin( mesh.rotation.y ) * 0.5 + 0.5;
+
+	//camera.position.x += ( mouseX - camera.position.x ) * .005;
+	//camera.position.y += ( - mouseY - camera.position.y ) * .01;
+
+	camera.lookAt( scene.position );
+
+	renderer.render( scene, camera );
+
+}
+
+function getPositionLeft(This) {
+	var el = This;
+	var pL = 0;
+	while(el) {
+		pL += el.offsetLeft;
+		el = el.offsetParent;
+	}
+	return pL
+}
+function getPositionTop(This) {
+	var el = This;
+	var pT = 0;
+	while(el) {
+		pT += el.offsetTop;
+		el = el.offsetParent;
+	}
+	return pT
+}
